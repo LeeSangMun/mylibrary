b3cc1fe1f8906db7d8000354ed73e81874f946a8
LeeSangMun <zbqm1919@naver.com>
2024-02-01 01:06:59 +0900

    [level 0] Title: 소인수분해, Time: 2.26 ms, Memory: 69 MB -BaekjoonHub

---
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        Set<Integer> set = new HashSet<>();
+        
+        while(n != 1) {
+        	for (int i = 2; i <= n; i++) {
+				if(n%i == 0) {
+					set.add(i);
+					n /= i;
+					break;
+				}
+			}
+        }
+        
+        return set.stream().sorted().mapToInt(Integer::valueOf).toArray();
+    }
+}
