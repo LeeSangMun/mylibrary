034c48c33c4ebf201e6087c5bed18028a21e0897
LeeSangMun <zbqm1919@naver.com>
2024-03-04 00:03:27 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 99.06 ms, Memory: 143 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -5,6 +5,7 @@ class Solution {
         for (int i = 0; i < numbers.length; i++) {
 			if(numbers[i]%2 == 0 || !BigInteger.valueOf(numbers[i]).testBit(1)) {
 				numbers[i]++;
+            
 			} else {
 				StringBuilder sb = new StringBuilder(("0" + Long.toBinaryString(numbers[i])));
 				int idx = sb.lastIndexOf("0");
