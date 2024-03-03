ecab4dcb9239063cfc666d46b8c97ff437d48b36
LeeSangMun <zbqm1919@naver.com>
2024-03-03 22:14:16 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 97.84 ms, Memory: 142 MB -BaekjoonHub

---
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -0,0 +1,18 @@
+import java.math.BigInteger;
+
+class Solution {
+    public long[] solution(long[] numbers) {
+        for (int i = 0; i < numbers.length; i++) {
+			if(numbers[i]%2 == 0 || !BigInteger.valueOf(numbers[i]).testBit(1)) {
+				numbers[i]++;
+			} else {
+				StringBuilder sb = new StringBuilder(("0" + Long.toBinaryString(numbers[i])));
+				int idx = sb.lastIndexOf("0");
+				sb.replace(idx, idx+2, "10");
+				numbers[i] = Long.valueOf(sb.toString(), 2);
+			}
+		}
+		
+		return numbers;
+    }
+}
