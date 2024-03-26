97c94d066bcfd79b30c8961dbe6f85cbf622087e
LeeSangMun <zbqm1919@naver.com>
2024-03-26 19:45:13 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -0,0 +1,18 @@
+SELECT he.EMP_NO, EMP_NAME
+    , 
+    CASE 
+        WHEN AVG(SCORE) >= 96 THEN 'S'
+        WHEN AVG(SCORE) >= 90 THEN 'A'
+        WHEN AVG(SCORE) >= 80 THEN 'B'
+        ELSE 'C'
+    END GRADE
+    , 
+    CASE 
+        WHEN AVG(SCORE) >= 96 THEN SAL * 0.2
+        WHEN AVG(SCORE) >= 90 THEN SAL * 0.15
+        WHEN AVG(SCORE) >= 80 THEN SAL * 0.1
+        ELSE 0
+    END BONUS
+FROM HR_EMPLOYEES he JOIN HR_GRADE hg ON he.EMP_NO = hg.EMP_NO
+GROUP BY he.EMP_NO, EMP_NAME
+ORDER BY he.EMP_NO
