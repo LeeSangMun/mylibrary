8d6cc799134d90ca3a96d5c8b833a74bad93a9a1
LeeSangMun <zbqm1919@naver.com>
2024-04-08 23:40:50 +0900

    [level 2] Title: 롤케이크 자르기, Time: 215.64 ms, Memory: 176 MB -BaekjoonHub

---
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] topping) {
+        int answer = 0;
+		Map<Integer, Integer> map = new HashMap<>();
+		Set<Integer> set1 = new HashSet<>(), set2 = new HashSet<>();
+
+		for (int i = 0; i < topping.length; i++) {
+			set1.add(topping[i]);
+			map.put(topping[i], map.getOrDefault(topping[i], 0) + 1);
+		}
+
+		for (int i = topping.length - 1; i >= 0; i--) {
+			map.put(topping[i], map.get(topping[i]) - 1);
+			set2.add(topping[i]);
+			if (map.get(topping[i]) == 0)
+				set1.remove(topping[i]);
+
+			if (set1.size() == set2.size()) {
+				answer++;
+			} else if (set1.size() < set2.size()) {
+				break;
+			} else {
+				answer = 0;
+			}
+		}
+
+		return answer;
+    }
+}
