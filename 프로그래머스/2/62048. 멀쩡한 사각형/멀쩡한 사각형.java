e4e10024a2ae1ad4fd88e3c25f9971ca8f877112
LeeSangMun <zbqm1919@naver.com>
2024-04-27 09:04:16 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 0.13 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
@@ -0,0 +1,26 @@
+class Solution {
+    public long solution(int w, int h) {
+        int n = 1;
+		long num = (long)w * h;
+
+		if (w == h)
+			return (long)w * w - w;
+		
+		boolean flag = true;
+		
+		while(flag) {
+			flag = false;
+			for (int i = 2; i <= Math.min(w, h); i++) {
+				if(w % i == 0 && h % i == 0) {
+					w /= i;
+					h /= i;
+					flag = true;
+					n *= i;
+					break;
+				}
+			}
+		}
+		
+		return num - (w + h - 1) * n;
+    }
+}
