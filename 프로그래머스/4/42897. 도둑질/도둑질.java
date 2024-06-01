9521f8d4a71fd00c5ac92feab1d8a775695d5541
LeeSangMun <zbqm1919@naver.com>
2024-06-01 13:48:16 +0900

    [level 4] Title: 도둑질, Time: 26.95 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.java
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -1,9 +1,23 @@
-#include <string>
-#include <vector>
-
-using namespace std;
-
-int solution(vector<int> money) {
-    int answer = 0;
-    return answer;
+class Solution {
+    public int solution(int[] money) {
+        int len = money.length;
+		int[][] dp = new int[2][len];
+		
+		if(len == 1) return money[0];
+		
+		dp[0][0] = dp[0][1] = money[0];
+		dp[1][0] = 0;
+		dp[1][1] = money[1];
+		
+		for (int i = 2; i < len; i++) {
+			if(i == len) {
+				dp[1][i] = Math.max(dp[1][i - 2] + money[i], dp[1][i - 1]);
+			} else {
+				dp[0][i] = Math.max(dp[0][i - 2] + money[i], dp[0][i - 1]);
+				dp[1][i] = Math.max(dp[1][i - 2] + money[i], dp[1][i - 1]);
+			}
+		}
+		
+        return Math.max(dp[0][len - 2], dp[1][len - 1]);
+    }
 }
